dcad813827be61845ace6302b61798f42d71d85e
KrisTenFour <109226404+KrisTenFour@users.noreply.github.com>
2024-10-16 16:39:50 +0200

    Add Dictionary component to App.js
    Plus add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import React from 'react';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Dictionary</h1>
+        <header><h1>Dictionary</h1></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>This page was coded by Kristen Namigai🧝🏻and is open-souced on GitHub and hosted on Netlify</footer>
       </div>
     </div>
   );
