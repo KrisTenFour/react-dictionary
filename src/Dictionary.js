6212fe138bbf61bc13c061302e0286a32baa5b4e
KrisTenFour <109226404+KrisTenFour@users.noreply.github.com>
2024-10-16 17:05:23 +0200

    Add search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,12 @@
 import React from "react";
+import "./Dictionary.css";
 
 export default function Dictionary() {
-    return <div>Hello from dictionary</div>
+    return (<div className="Dictionary">
+        <h2>What word are you looking for?</h2>
+        <form>
+            <input type="search" placeholder="Search..." autofocus />
+            <input type="submit" value="Search" />
+        </form>
+    </div>)
 }
