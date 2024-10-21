a2cf7f260eeca88b8fdf2fbd8b2e50fa82440df6
KrisTenFour <109226404+KrisTenFour@users.noreply.github.com>
2024-10-21 12:12:48 +0200

    Add conditional display to Results component

--- src/Results.js
+++ src/Results.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "./Results.css";
 
-export default function Results() {
-    return (
-        <div>
-            <h3>Hey from Results</h3>
-        </div>)
+export default function Results(props) {
+    if (props.results) {
+        return <div><h3>Hey from {props.results}</h3></div>
+    } else { return null; }
 
 }
