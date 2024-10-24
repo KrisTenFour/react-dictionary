68ca9441fbdc05f3cf20331ebbd5fbc4fe990e9f
KrisTenFour <109226404+KrisTenFour@users.noreply.github.com>
2024-10-24 13:41:01 +0200

    Update CSS

--- src/Results.js
+++ src/Results.js
@@ -7,15 +7,23 @@ export default function Results(props) {
     if (props.results) {
         return (
             <div className="Results">
-                <h3>{props.results.word}</h3>
-                <Phonetic phonetic={props.results.phonetic} />
-                {props.results.meanings.map(function (meanings, index) {
-                    return (
-                        <div key={index}>
-                            <Meaning meaning={meanings} />
-                        </div>
-                    );
-                })}
+                <section>
+                    <div className="word">
+                        <h3>{props.results.word}</h3>
+                        <Phonetic phonetic={props.results.phonetic} />
+                    </div>
+                </section>
+                <section>
+                    <div className="definition">
+                        {props.results.meanings.map(function (meanings, index) {
+                            return (
+                                <div key={index}>
+                                    <Meaning meaning={meanings} />
+                                </div>
+                            );
+                        })}
+                    </div>
+                </section>
             </div>)
     } else { return null; }
 
