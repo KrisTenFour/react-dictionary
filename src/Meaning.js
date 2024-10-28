b18037f6ef4f496282850c3980ae6bc9b7356add
KrisTenFour <109226404+KrisTenFour@users.noreply.github.com>
2024-10-28 21:06:37 +0100

    Add text for words that aren't found by API

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,13 +4,22 @@ import Example from "./Example";
 import "./Meaning.css";
 
 export default function Meaning(props) {
-    return (
-        <div className="Meaning">
-            <h5>{props.meaning.partOfSpeech}</h5>
-            <p className="definition"><em>{props.meaning.definition}</em></p>
-            <Example example={props.meaning.example} />
-            <Synonym synonym={props.meaning.synonyms} />
-        </div >
-    );
+    if (props.meaning) {
+        return (
+            <div className="Meaning">
+                <h5>{props.meaning.partOfSpeech}</h5>
+                <p className="definition"><em>{props.meaning.definition}</em></p>
+                <Example example={props.meaning.example} />
+                <Synonym synonym={props.meaning.synonyms} />
+            </div >
+        );
+    } else {
+        return (
+            <div className="not-found">
+                <p>Oops I stopped loading! Try another word!</p>
+            </div>
+        )
+    }
+
 
 }
