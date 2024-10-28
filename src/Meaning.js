402dd2cf7c7c732419ced1b7f5631dd1e2189c28
KrisTenFour <109226404+KrisTenFour@users.noreply.github.com>
2024-10-28 18:27:31 +0100

    Update CSS

--- src/Meaning.js
+++ src/Meaning.js
@@ -7,10 +7,10 @@ export default function Meaning(props) {
     return (
         <div className="Meaning">
             <h5>{props.meaning.partOfSpeech}</h5>
-            <p className="definition">{props.meaning.definition}</p>
+            <p className="definition"><em>{props.meaning.definition}</em></p>
             <Example example={props.meaning.example} />
             <Synonym synonym={props.meaning.synonyms} />
-        </div>
+        </div >
     );
 
 }
