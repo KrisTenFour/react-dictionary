41823040db49bdcaf1e981ba1ee908200dfd6977
KrisTenFour <109226404+KrisTenFour@users.noreply.github.com>
2024-10-28 21:09:06 +0100

    Remove text for words that aren't found by API, not working

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,22 +4,12 @@ import Example from "./Example";
 import "./Meaning.css";
 
 export default function Meaning(props) {
-    if (props.meaning) {
-        return (
-            <div className="Meaning">
-                <h5>{props.meaning.partOfSpeech}</h5>
-                <p className="definition"><em>{props.meaning.definition}</em></p>
-                <Example example={props.meaning.example} />
-                <Synonym synonym={props.meaning.synonyms} />
-            </div >
-        );
-    } else {
-        return (
-            <div className="not-found">
-                <p>Oops I stopped loading! Try another word!</p>
-            </div>
-        )
-    }
-
-
+    return (
+        <div className="Meaning">
+            <h5>{props.meaning.partOfSpeech}</h5>
+            <p className="definition"><em>{props.meaning.definition}</em></p>
+            <Example example={props.meaning.example} />
+            <Synonym synonym={props.meaning.synonyms} />
+        </div >
+    );
 }
