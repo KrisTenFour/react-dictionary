fa4e04a79c727ba9cc49ba5695e75bdc8e5f5047
KrisTenFour <109226404+KrisTenFour@users.noreply.github.com>
2024-10-31 13:06:17 +0100

    Update Synonym component to new API

--- src/Meaning.js
+++ src/Meaning.js
@@ -10,7 +10,7 @@ export default function Meaning(props) {
             <h5>{props.meaning.partOfSpeech}</h5>
             <p className="definition"><em>{props.meaning.definitions[0].definition}</em></p>
             <Example example={props.meaning.definitions[0].example} />
-            <Synonym synonym={props.meaning.definitions[0].synonyms} />
+            <Synonym synonym={props.meaning.synonyms} />
         </div >
     );
 }
