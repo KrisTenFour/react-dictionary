7233bc9c4f00422726d0db999c7d13d3739ce7ab
KrisTenFour <109226404+KrisTenFour@users.noreply.github.com>
2024-10-31 12:59:16 +0100

    Update Meaning component to new API

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,12 +4,13 @@ import Example from "./Example";
 import "./Meaning.css";
 
 export default function Meaning(props) {
+    console.log(props);
     return (
         <div className="Meaning">
             <h5>{props.meaning.partOfSpeech}</h5>
-            <p className="definition"><em>{props.meaning.definition}</em></p>
-            <Example example={props.meaning.example} />
-            <Synonym synonym={props.meaning.synonyms} />
+            <p className="definition"><em>{props.meaning.definitions[0].definition}</em></p>
+            <Example example={props.meaning.definitions[0].example} />
+            <Synonym synonym={props.meaning.definitions[0].synonyms} />
         </div >
     );
 }
